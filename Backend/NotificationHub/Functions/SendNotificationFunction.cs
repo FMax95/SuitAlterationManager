3d4345d5983e4b4090fb0f6d2f9eef595c4cae76
FMax95 <massimiflavio95@gmail.com>
2022-07-24 12:02:26 +0200

    feat(backend) add notification hub

---
+++ Backend/NotificationHub/Functions/SendNotificationFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Newtonsoft.Json;
+
+namespace NotificationHub
+{
+    public class SendNotificationFunction
+    {
+        [Function("SendNotification")]
+        public static async Task SendNotification(
+            [ServiceBusTrigger(topicName: "AlterationFinished", subscriptionName:"emailsubscription", Connection = "ServiceBusConnectionString")] string item,        
+            FunctionContext executionContext)
+        {
+            //var obj = JsonConvert.DeserializeObject<object>(item);
+            throw new NotImplementedException();
+        }
+    }
+}
